25396f60820820a3f4ea5449c6eef33c6c073a0c
xi-j <xilinjiang28@gmail.com>
2026-01-11 02:53:31 +0000

    Deploying to gh-pages from @ xi-j/xi-j.github.io@f2035f5bf8ee26e49a17bab04dfb45ae1f332bd5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -106,7 +106,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%78%6A%32%32%38%39@%63%6F%6C%75%6D%62%69%61.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -116,18 +116,11 @@ ninja.data = [{
           window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=FQrkPUwAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
